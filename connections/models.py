9ff595c7e26ce1926a42dbe9a72a62b29907b630
Navin <navinlubhaya@gmail.com>
2018-05-12 19:34:08 +0100

    choices for models

--- connections/models.py
+++ connections/models.py
@@ -3,6 +3,7 @@ from accounts.models import FarmUser
 
 # Create your models here.
 
+
 #Client Table
 class ClientTable(models.Model):
     client_id = models.CharField(max_length=50, default='')
@@ -15,11 +16,32 @@ class ClientTable(models.Model):
 
 #Connection Table
 class ConnectionTable(models.Model):
+    MESSAGE_CHOICES = (
+        ('movement', 'movement'),
+        ('execution', 'execution'),
+        ('financial', 'financial'),
+    )
+
+    DIRECTION_CHOICES = (
+        ('to partner', 'to partner'),
+        ('from partner', 'from partner'),
+    )
+
+    STATUS_CHOICES = (
+        ('awaiting approval', 'awaiting approval'),
+        ('approved', 'approved'),
+        ('rejected', 'rejected'),
+    )
+
+
     user = models.ForeignKey(FarmUser, on_delete=models.CASCADE)
-    message_type = models.CharField(max_length=50, default='')
-    direction = models.CharField(max_length=50, default='')
+    message_type = models.CharField(max_length=50, choices=MESSAGE_CHOICES,default='')
+    direction = models.CharField(max_length=50, choices=DIRECTION_CHOICES, default='')
     client = models.ForeignKey(ClientTable, on_delete=models.CASCADE)
-    status = models.CharField(max_length=50, default='awaiting approval')
+    status = models.CharField(max_length=50, choices=STATUS_CHOICES, default='awaiting approval')
 
     def __str__(self):          #diplay object as string
         return '%s' % (self.user.business_name)
+
+    class Meta:
+        unique_together = ('user','message_type','direction','client')
