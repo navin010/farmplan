1f8a6eb2caa39d753649ddcd5cd1a5226981dd87
Navin <navinlubhaya@gmail.com>
2018-05-12 14:19:01 +0100

    mods for tables look ok

--- connections/admin.py
+++ connections/admin.py
@@ -2,5 +2,20 @@ from django.contrib import admin
 from connections.models import ClientTable, ConnectionTable
 
 # Register your models here.
-admin.site.register(ClientTable)
-admin.site.register(ConnectionTable)
+#admin.site.register(ClientTable)
+#admin.site.register(ConnectionTable)
+
+
+
+#Client Admin Section
+class ClientTableAdmin(admin.ModelAdmin):
+    list_display = ('client_id', 'client_name', 'f4f_code')
+
+admin.site.register(ClientTable, ClientTableAdmin)
+
+
+#Connection Admin Section
+class ConnectionTableAdmin(admin.ModelAdmin):
+    list_display = ('user', 'message_type', 'direction', 'client', 'status')
+
+admin.site.register(ConnectionTable, ConnectionTableAdmin)
